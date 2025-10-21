2443cb9512fd89cf0e60059fdc0e58f39ea1bfbf
mikkel102454 <mikkeliversen09@outlook.dk>
2025-10-21 10:58:02 +0200

    db optimizing

--- lockr/src/service/data.cpp
+++ lockr/src/service/data.cpp
@@ -4,6 +4,7 @@
 
 #include "nlohmann/json.hpp"
 #include <string>
+#include <bsoncxx/json.hpp>
 
 namespace lockr {
     static int maxData = 1024*1024; // 1MB
@@ -22,25 +23,21 @@ namespace lockr {
                   ));
         if (!doc) return false;
 
-        auto v = doc->view();
-        outJson = nlohmann::json::parse(std::string{ v["data"].get_string().value });
+        const auto dataView = doc->view()["data"].get_document().view();
+        outJson = nlohmann::json::parse(bsoncxx::to_json(dataView));
         return true;
     }
 
     bool ReplaceData(const std::string& userId, const nlohmann::json& json, const std::string& companyId) {
-        const auto bson = nlohmann::json::to_bson(json);
-        if (bson.size() > maxData) return false;
+        const bsoncxx::document::value jsonDoc = bsoncxx::from_json(json.dump());
+        if (jsonDoc.view().length() > maxData) return false;
 
         DB::UpdateOne("data", bsoncxx::builder::basic::make_document(
             bsoncxx::builder::basic::kvp("user_id", userId),
             bsoncxx::builder::basic::kvp("company_id", companyId)
             ),
             bsoncxx::builder::basic::make_document(
-                bsoncxx::builder::basic::kvp("data",  bsoncxx::types::b_binary{
-                bsoncxx::binary_sub_type::k_binary,
-                static_cast<uint32_t>(bson.size()),
-                bson.data()
-                })
+                bsoncxx::builder::basic::kvp("data", jsonDoc.view())
             ));
 
         return true;
